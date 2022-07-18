42fee2bf65384920c3b2cedd679ae4d66d90385b
zinodev0 <zinodev0@gmail.com>
2022-07-18 20:35:40 +0100

    Added material ui

---
+++ pages/rock-paper-scissors/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RockPaperScissors = () => {
+  return (
+    <div className="container">
+      RockPaperScissors
+      <p>Rock</p>
+      <p>Paper</p>
+      <p>Scissors</p>
+    </div>
+  );
+};
+
+export default RockPaperScissors;
