20965e54077fcb3578fa9b5ef5a39ac0d237f7b1
zinodev0 <zinodev0@gmail.com>
2022-08-12 12:28:11 +0100

    Started Guess the number Game logic

--- pages/guess-the-number/index.jsx
+++ pages/guess-the-number/index.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../Layout";
 
 const index = () => {
   const [userGuess, setUserGuess] = useState(null);
   const [compGuess, setCompGuess] = useState(null);
+  const [hint, setHint] = useState("");
 
+  useEffect(() => {
+    setCompGuess(Math.floor(Math.random() * 99) + 1);
+  }, []);
+
+  const gamePlay = () => {
+    if (userGuess > compGuess) {
+      setHint("Go Lower");
+    } else if (userGuess < compGuess) {
+      setHint("Go Higher");
+    } else if (userGuess === compGuess) {
+      setHint(`CONGRATULATIONS THE ANSWER WAS ${compGuess}`);
+    } else return;
+  };
   return (
     <Layout>
       <div>
@@ -18,13 +32,29 @@ const index = () => {
 
         <input
           value={userGuess}
-          onChange={(e) => setUserGuess(e.target.value)}
-          type="text"
+          onChange={(e) => setUserGuess(Number(e.target.value))}
+          type="number"
           placeholder="Enter a number between 0 and 100"
         />
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={gamePlay}>
+          Submit
+        </button>
+
+        {userGuess !== null && (
+          <h3 style={{ color: "blue" }}>
+            {" "}
+            You guessed {userGuess}, {userGuess !== compGuess && hint}{" "}
+          </h3>
+        )}
+        {userGuess !== null && (userGuess > 99 || userGuess < 1) && (
+          <p style={{ color: "red" }}>
+            Number must be greater than 0 and less than 100
+          </p>
+        )}
 
         <h2>User Guess: {userGuess}</h2>
+        <h2>User Guess Type: {typeof userGuess}</h2>
+        <h2>Comp Guess: {compGuess}</h2>
       </div>
     </Layout>
   );
